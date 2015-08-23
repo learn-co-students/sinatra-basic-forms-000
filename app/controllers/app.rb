0e76b3a37b2cdda36be68dd4554ad8ef0b4dde9a
mbrickenworthe <adamwinters456@gmail.com>
2015-08-23 18:49:56 -0400

    Creating a simple Sintra app that can do updates to data

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -10,6 +10,26 @@ class App < Sinatra::Base
     @song = Song.find(params[:id])
     erb :'songs/show'
   end
+
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    erb :'songs/edit'
+  end
+
+  post '/songs/:id' do
+    @song = Song.find(params[:id])
+    song = Song.find(params[:id])
+    @song.update_attributes(params[:song])
+  # redirect to("/songs/#{@song.id}")
+    erb :'songs/show'
+  end
+
+  # post '/songs/:id/edit' do
+  #   @song = Song.find(params[:id])
+  #   song = Song.find(params[:id])
+  #   @song.update_attributes(params[:song])
+  #   redirect to("/songs/#{@song.id}")
+  # end
   
 end
 
